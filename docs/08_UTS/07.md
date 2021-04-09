28b097f30a6de058b55cf35f55adf2181dc4b3d1
safa54f4 <34050656+safa54f4@users.noreply.github.com>
2021-04-09 20:28:52 +0700

    uts

---
+++ docs/08_UTS/07.md
@@ -0,0 +1,17 @@
+# 07 - Context dan HOC di ReactJS
+
+## Tujuan Pembelajaran
+
+1. Konsep Context dan manfaatnya
+2. Konsep HOC di ReactJS
+
+## Hasil Praktikum
+
+## Praktikum 
+
+- ![praktikum1](img/Hasil7-1.PNG)
+- ![praktikum2](img/Hasil7-2.PNG)
+- ![praktikum3](img/Hasil7-3.PNG)
+
+Untuk melihat kode Praktikum 1 langsung klik tautan berikut [Link Code](../../src/07_Context_dan_HOC)
+
---
+++ src/08_UTS/Praktikum_2/index.js
@@ -0,0 +1,36 @@
+import ReactDOM from 'react-dom';
+import Counter from './App.js';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
