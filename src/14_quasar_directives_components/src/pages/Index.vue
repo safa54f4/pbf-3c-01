bd8fcad660d9fd8a11b966f85118363d61a36d0d
safa54f4 <34050656+safa54f4@users.noreply.github.com>
2021-06-23 11:22:14 +0700

    14

---
+++ src/14_quasar_directives_components/src/pages/Index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="q-pa-md row justify-center">
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn color="primary" icon="mail" label="On Left" />
+      <q-btn color="secondary" icon-right="mail" label="On Right" />
+      <q-btn color="red" icon="mail" icon-right="send" label="On Left and Right" />
+      <br />
+      <q-btn icon="phone" label="Stacked" stack glossy color="purple"/>
+    </div>
+  </div>
+</template>
+
+<script>
+const colors = [
+  "primary",
+  "amber",
+  "secondary",
+  "orange",
+  "accent",
+  "lime",
+  "cyan",
+  "purple",
+  "brown",
+  "blue"
+];
+
+export default {
+  data() {
+    return {
+      color: colors[0],
+      index: 0
+    };
+  },
+
+  computed: {
+    classes() {
+      return `bg-${this.color}`;
+    }
+  },
+
+  mounted() {
+    this.timer = setInterval(() => {
+      this.index = (this.index + 1) % colors.length;
+      this.color = colors[this.index];
+    }, 3000);
+  },
+
+  beforeDestroy() {
+    clearInterval(this.timer);
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.container
+  border-radius: 3px
+  cursor: pointer
+  transition: background 1.5s
+  height: 150px
+  width: 80%
+  max-width: 500px
+</style>
